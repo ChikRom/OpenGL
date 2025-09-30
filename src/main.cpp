c0bb39b077f4bc06854afe4f09abc3047104be2a
Roman <romun2006@mail.ru>
2025-09-30 19:57:29 +0300

    Learn the basis of the Graphic Pipeline, write VAO, VBO, vertex shader, fragment shader and draw a viollet triangle on the yellow screen

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,29 @@
 // callback function which executes every time when the window size is change
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
-// funcion which check if user pressed the escape key to close the window
+// function which check if user pressed the escape key to close the window
 void processInput(GLFWwindow* window);
+
+
+
+
+//Vertex shader
+const char* vertexShaderSource =
+"#version 460 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+"}\0";
+
+//Fragment shader
+const char* fragmentShaderSource =
+"#version 460 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(0.3568f, 0.0f, 0.7294f, 1.0f);\n"
+"}\n\0";
 
 int main()
 {
@@ -18,8 +39,9 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+
 	// create the window
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1600, 1000, "OpenGL", NULL, NULL);
 
 	if (window == NULL)
 	{
@@ -40,6 +62,63 @@ int main()
 	// resize by registering it:
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	
+	//create Vertex shader
+	//attach the Vertex Shader source code to the shader object
+	// and compile the Vertex Shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//create Fragment shader
+	//attach the Fragment Shader source code to the shader object
+	// and compile the Fragment Shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+
+	//Create Shader program, attach our shaders to program and link them
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+
+	//delete shaders
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//vertex data
+	float vertices[]{
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f,  0.0f
+	};
+
+	//Generate Vertex Array Object and Vertex Buffer Object
+	unsigned int VBO,VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	//bind our Vertex Array Object which will store our attributes configuration and current VBO
+	glBindVertexArray(VAO);
+
+	//bind our VBO with target buffer, send our vertex data to the VBO and specify
+	// how we want to manage this data (to better allocation of memory)
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// set our vertex attributes for vertex shader and active vertex attribute with index 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//unbind VAO and VBO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	//render loop
 	while (!glfwWindowShouldClose(window))
@@ -49,17 +128,24 @@ int main()
 
 		// set clear values for the colour buffers
 		// and clear buffers to preset values
-		glClearColor(0.3568f, 0.0f, 0.7294f, 1.0f); // violet
+		glClearColor(1.0f, 1.0f, 0.0f, 1.0f); // yellow
+		//glClearColor(0.3568f, 0.0f, 0.7294f, 1.0f); // violet
 		glClear(GL_COLOR_BUFFER_BIT);
 
 
-
+		//activate the shader program, bind our VAO and draw triangle from 0-indexed vertice
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES,0,3);
 		// swap back and front buffers for avoiding artifacts
 		// and check the events from user (mouse, keyboard)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
+	// delete VAO, VBO and shader program
+	glDeleteVertexArrays(1,&VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 	glfwTerminate();
 
 	return 0;
@@ -78,5 +164,5 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true); // set the value of the close flag of the specified
+		glfwSetWindowShouldClose(window, true); // set the value of the close flag of the specified window
 }
