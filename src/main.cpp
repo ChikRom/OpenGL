0a16e3f3e532d81e29738bc81d6c67f6c1ed7467
Roman <romun2006@mail.ru>
2025-10-01 17:38:37 +0300

    Learned new things about shaders, GLSL, reallized the connection between vertex and fragment shaders, learned uniforms, made a color-changing rectangle, understand the logic of vertex attributes and how to specify them, draw a rainbow triangle using a fragment interpolation (Position of fragment determine its colour)

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,22 @@ void processInput(GLFWwindow* window);
 const char* vertexShaderSource =
 "#version 460 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColors;\n"
+"out vec4 vertexColors;\n"
 "void main()\n"
 "{\n"
-"	gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+"	gl_Position = vec4(aPos,1.0);\n"
+"	vertexColors = vec4(aColors,1.0f);\n"
 "}\0";
 
 //Fragment shader
 const char* fragmentShaderSource =
 "#version 460 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColors;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(0.3568f, 0.0f, 0.7294f, 1.0f);\n"
+"	FragColor = vertexColors;\n"
 "}\n\0";
 
 int main()
@@ -62,7 +66,6 @@ int main()
 	// resize by registering it:
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	
 	//create Vertex shader
 	//attach the Vertex Shader source code to the shader object
 	// and compile the Vertex Shader
@@ -93,19 +96,26 @@ int main()
 	glDeleteShader(fragmentShader);
 
 	//vertex data
+	//float vertices[]
+	//{
+	//	-0.5f, -0.5f, 0.0f,
+	//	 0.5f, -0.5f, 0.0f,
+	//	 0.5f,  0.5f, 0.0f,
+	//	//-0.5f,  0.5f, 0.0f
+	//};
+
 	float vertices[]
-	{
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f
+	{	// vertices				// colors
+		-0.5f, -0.5f, 0.0f,		0.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,
+		 0.0f,  0.5f, 0.0f,		1.0f, 0.0f, 0.0f
 	};
 
 	// indices data
 	unsigned int indices[] =
 	{
 		0,1,2,
-		0,3,2
+		//0,3,2
 	};
 
 	//Generate Vertex Array Object, Vertex Buffer Object and Element Buffer Object
@@ -113,6 +123,7 @@ int main()
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
+
 
 	//bind our Vertex Array Object which will store our attributes configuration and current VBO
 	glBindVertexArray(VAO);
@@ -128,9 +139,11 @@ int main()
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
-	// set our vertex attributes for vertex shader and active vertex attribute with index 0
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// set our vertex and colour attributes for vertex shader and active vertex attribute with index 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	//unbind VAO, VBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -153,12 +166,20 @@ int main()
 		//glClearColor(0.3568f, 0.0f, 0.7294f, 1.0f); // violet
 		glClear(GL_COLOR_BUFFER_BIT);
 
-
-		//activate the shader program, bind our VAO and draw triangle from 0-indexed vertices
+		//activate the shader program
 		glUseProgram(shaderProgram);
+
+		// change the colour in the moment with uniform
+		/*float timeValue = glfwGetTime();
+		float blueValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.3568f, 0.0f, blueValue,1.0f);*/
+
+
+		//bind our VAO and draw triangle from 0-indexed vertices
+		//glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES,0,3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 		// swap back and front buffers for avoiding artifacts
 		// and check the events from user (mouse, keyboard)
 		glfwSwapBuffers(window);
@@ -170,6 +191,7 @@ int main()
 	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 	glfwTerminate();
+
 
 	return 0;
 }
